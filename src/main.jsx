0f1dafc3b349d04a76d89ee65b71e2f6ca8add00
chimae <chimandukwe.r@gmail.com>
2025-06-05 00:09:44 +0000

    Completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "normalize.css";
+import "./index.css";
+import App from './App.jsx'
+import { store } from "./store";
+import { Provider } from "react-redux";
+
+ createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App tab="home" />
+  </Provider>
+);
+
